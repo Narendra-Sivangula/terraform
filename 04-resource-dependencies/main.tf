2effb9527e1e323176c9c7fba7ffe202a8a6d6f3
narendrasre <narendrasivangula@gmail.com>
2026-08-29 15:01:19 +0000

    ADDED LESSON 04 RESOURCE DEPENDENCIES

---
+++ 04-resource-dependencies/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                     = "ap-south-2"
+  access_key                 = "test"
+  secret_key                 = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  skip_metadata_api_check    = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+  s3_use_path_style = true
+}
+
+resource "aws_s3_bucket" "dependency_bucket" {
+  bucket = "narendra-04-lesson-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.dependency_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
